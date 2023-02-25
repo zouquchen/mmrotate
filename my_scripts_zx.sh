91d9d7bd145dea7386c1fd564441b096a9af99e0
xiaoyanlu <10971659+xiaoyanlu@user.noreply.gitee.com>
2023-02-25 21:30:06 +0800

    ignore label container-crane of dota1.5

--- my_scripts_zx.sh
+++ my_scripts_zx.sh
@@ -11,11 +11,11 @@
 
 # 训练
 # 临时设置GPU
-set CUDA_VISIBLE_DEVICES=1
+# set CUDA_VISIBLE_DEVICES=1
 
 # oriented_reppoints_r50
-# python tools/train.py "./configs/oriented_reppoints/oriented_reppoints_r50_fpn_1x_dota_le135.py" --work-dir "D:/zqc2/1-code/mmrotate_dir/oriented_reppoints_r50_fpn_1x_dota_le135/20230223"
+python tools/train.py "./configs/oriented_reppoints/oriented_reppoints_r50_fpn_1x_dota_le135.py" --work-dir "D:/zqc2/1-code/mmrotate_dir/oriented_reppoints_r50_fpn_1x_dota_le135/20230224"
 
 # rotated_reppoints
-python tools/train.py "./configs/rotated_reppoints/rotated_reppoints_r50_fpn_1x_dota_le135.py" --work-dir "D:/zqc2/1-code/mmrotate_dir/rotated_reppoints_r50_fpn_1x_dota_le135/20230223"
+python tools/train.py "./configs/rotated_reppoints/rotated_reppoints_r50_fpn_1x_dota_le135.py" --work-dir "D:/zqc2/1-code/mmrotate_dir/rotated_reppoints_r50_fpn_1x_dota_le135/20230224"
 
